d99d52f6c26029c7fa9f0f0f8285121f7d86a457
Martin Torp <toldamtorp@gmail.com>
2020-11-24 13:23:50 +0100

    copy wasi clang compiler to afl_wasm repo in the prepare magma script

--- init-workspace.sh
+++ init-workspace.sh
@@ -4,3 +4,5 @@ git clone git@github.com:mtorp/magma-WASM.git
 git clone git@github.com:mtorp/wasm-project.git
 git clone git@github.com:mtorp/AFL-wasm.git
 git clone git@github.com:mtorp/wasabi.git
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
+tar xvf wasi-sdk-11.0-linux.tar.gz
--- prepare-magma.sh
+++ prepare-magma.sh
@@ -7,5 +7,8 @@ cp wasm-project/wasm_instrumenter/target/release/afl_branch AFL-wasm/
 cp wasm-project/wasm_instrumenter/target/release/canaries AFL-wasm/
 
 # copy afl_wasm to magma fuzzers
-rm -rf magma/fuzzers/afl_wasm
-rsync --links -r --exclude='.git' AFL-wasm/* magma/fuzzers/afl_wasm
+rm -rf magma/fuzzers/afl_wasm/repo
+mkdir magma/fuzzers/afl_wasm/repo
+rsync --links -r --exclude='.git' AFL-wasm/* magma/fuzzers/afl_wasm/repo
+mkdir magma/fuzzers/afl_wasm/repo/wasi_sdk
+cp -r wasi-sdk-11.0/* magma/fuzzers/afl_wasm/repo/wasi_sdk
