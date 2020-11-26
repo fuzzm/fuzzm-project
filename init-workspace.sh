39bf0e43a75523a7cdd14d7272c9e519d766d40c
Martin Torp <toldamtorp@gmail.com>
2020-11-26 15:41:28 +0100

    download zlib during init-workspace

--- init-workspace.sh
+++ init-workspace.sh
@@ -6,3 +6,5 @@ git clone git@github.com:mtorp/AFL-wasm.git
 git clone git@github.com:mtorp/wasabi.git
 wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
 tar xvf wasi-sdk-11.0-linux.tar.gz
+wget https://zlib.net/zlib-1.2.11.tar.gz
+tar xvf zlib-1.2.11.tar.gz
