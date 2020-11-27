d62532ea01f4d3a22165ab8498bf1186d927f394
Martin <toldamtorp@gmail.com>
2020-11-27 10:15:49 +0100

    clone magma-WASM into a folder named 'magma'

--- init-workspace.sh
+++ init-workspace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e 
-git clone git@github.com:mtorp/magma-WASM.git
+git clone git@github.com:mtorp/magma-WASM.git magma
 git clone git@github.com:mtorp/wasm-project.git
 git clone git@github.com:mtorp/AFL-wasm.git
 git clone git@github.com:mtorp/wasabi.git
