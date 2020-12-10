bcd78f8c02060ca9f4d0e3b645ecc74b6091de17
Martin <toldamtorp@gmail.com>
2020-12-10 07:33:23 +0100

    add LAVA-native to workspace

--- init-workspace.sh
+++ init-workspace.sh
@@ -5,6 +5,7 @@ git clone git@github.com:mtorp/wasm-project.git
 git clone git@github.com:mtorp/AFL-wasm.git
 git clone git@github.com:mtorp/wasabi.git
 git clone git@github.com:mtorp/LAVA-M.git
+git clone git@github.com:mtorp/LAVA-native.git
 wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
 tar xvf wasi-sdk-11.0-linux.tar.gz
 wget https://zlib.net/zlib-1.2.11.tar.gz
