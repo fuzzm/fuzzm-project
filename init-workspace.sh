cd7896430da3c2d830a1ebdbb34f1d15d577a278
Martin <toldamtorp@gmail.com>
2020-12-16 09:30:09 +0100

    remove the wasabi clone from init-workspace since wasabi is not a direct dependency of the wasm_instrumenter

--- init-workspace.sh
+++ init-workspace.sh
@@ -3,7 +3,6 @@ set -e
 git clone git@github.com:mtorp/magma-WASM.git magma
 git clone git@github.com:mtorp/wasm-project.git
 git clone git@github.com:mtorp/AFL-wasm.git
-git clone git@github.com:mtorp/wasabi.git
 git clone git@github.com:mtorp/LAVA-M.git
 git clone git@github.com:mtorp/LAVA-native.git
 wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
