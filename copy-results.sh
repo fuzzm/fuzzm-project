5e0cbd30224bded749fcda687c4f48a3f829b43d
Martin Torp <toldamtorp@gmail.com>
2021-01-07 14:54:42 +0100

    script for copying the existing fuzzing results into the AFL-wasm programs folder

---
+++ copy-results.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp wasm-project/fuzzing_results/opj_compress_17h/opj_compress-wasm.zip AFL-wasm/programs/opj_compress-wasm/
+(cd AFL-wasm/programs/opj_compress-wasm/ && unzip -o opj_compress-wasm.zip && cp -r opj_compress-wasm/findings/* findings)
+
+cp wasm-project/fuzzing_results/libtiff_2d/findings.zip AFL-wasm/programs/pal2rgb-wasm/
+(cd AFL-wasm/programs/pal2rgb-wasm/ && unzip -o findings.zip && cp -r findings/* findings)
+
+cp wasm-project/fuzzing_results/pdfresurrect-dec-27/findings-wasm.zip AFL-wasm/programs/pdfresurrect-wasm/
+(cd AFL-wasm/programs/pdfresurrect-wasm/ && unzip -o findings-wasm.zip && cp -r findings/* findings)
