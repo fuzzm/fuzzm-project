d11bfa27ec43a58c1f7638ba1f229a7174708e0b
Martin Torp <toldamtorp@gmail.com>
2021-01-18 16:17:52 +0100

    update copy-results to use newest opj_compress results

--- copy-results.sh
+++ copy-results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp wasm-project/fuzzing_results/opj_compress_28h/opj_compress-wasm.zip AFL-wasm/programs/opj_compress-wasm/
-(cd AFL-wasm/programs/opj_compress-wasm/ && unzip -o opj_compress-wasm.zip && cp -r opj_compress-wasm/findings/* findings)
+cp wasm-project/fuzzing_results/opj_compress_54h/findings-wasm.zip AFL-wasm/programs/opj_compress-wasm/
+(cd AFL-wasm/programs/opj_compress-wasm/ && unzip -o findings-wasm.zip && cp -r opj_compress-wasm/findings/* findings)
 
 cp wasm-project/fuzzing_results/libtiff_2d/findings.zip AFL-wasm/programs/pal2rgb-wasm/
 (cd AFL-wasm/programs/pal2rgb-wasm/ && unzip -o findings.zip && cp -r findings/* findings)
