0096c3578c3e49bfe41951a243f0ea196c6324ff
Martin Torp <toldamtorp@gmail.com>
2021-02-01 11:11:12 +0100

    copy newest libpng results in copy-results script

--- copy-results.sh
+++ copy-results.sh
@@ -7,3 +7,7 @@ cp wasm-project/fuzzing_results/libtiff_4d_new/findings-wasm.zip AFL-wasm/progra
 
 cp wasm-project/fuzzing_results/pdfresurrect-dec-27/findings-wasm.zip AFL-wasm/programs/pdfresurrect-wasm/
 (cd AFL-wasm/programs/pdfresurrect-wasm/ && unzip -o findings-wasm.zip && cp -r findings/* findings)
+
+cp wasm-project/fuzzing_results/libpng_3h/findings-wasm.zip AFL-wasm/programs/pnm2png-wasm
+(cd AFL-wasm/programs/pnm2png-wasm && unzip -o findings-wasm.zip && cp -r findings/* findings)
+
