bdef045e980ff395a049d4b4a0032ab97d50f067
Martin Torp <toldamtorp@gmail.com>
2021-02-02 11:39:03 +0100

    use libpng 27H results in the copy-results script

--- copy-results.sh
+++ copy-results.sh
@@ -8,6 +8,6 @@ cp wasm-project/fuzzing_results/libtiff_4d_new/findings-wasm.zip AFL-wasm/progra
 cp wasm-project/fuzzing_results/pdfresurrect-dec-27/findings-wasm.zip AFL-wasm/programs/pdfresurrect-wasm/
 (cd AFL-wasm/programs/pdfresurrect-wasm/ && unzip -o findings-wasm.zip && cp -r findings/* findings)
 
-cp wasm-project/fuzzing_results/libpng_3h/findings-wasm.zip AFL-wasm/programs/pnm2png-wasm
+cp wasm-project/fuzzing_results/libpng_27h/findings-wasm.zip AFL-wasm/programs/pnm2png-wasm
 (cd AFL-wasm/programs/pnm2png-wasm && unzip -o findings-wasm.zip && cp -r findings/* findings)
 
