708cdb44c7d20da53f818c2e5739584db738955f
Martin Torp <toldamtorp@gmail.com>
2021-04-07 20:12:54 +0200

    update init-workspace script

--- init-workspace.sh
+++ init-workspace.sh
@@ -3,3 +3,8 @@ set -e
 wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
 tar xvf wasi-sdk-11.0-linux.tar.gz
 npm install
+
+(cd AFL-wasm && make)
+(cd wasm_instrumenter && cargo build --release)
+(cd benchmark-runner && npm i && npm run build)
+(cd benchmarks && ./compile-all.sh)
